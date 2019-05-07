4944503ae0661cd2636a31a9ed618f34d4078b94
KimMinJe <raimer448@google.com>
2019-05-08 03:58:15 +0900

    speech

---
+++ DR0508/DR0504/app/src/main/java/com/example/dr0504/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.dr0504;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView Notlog, register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Notlog = (TextView)findViewById(R.id.NotLogin);
+        register = (TextView)findViewById(R.id.register);
+
+        Notlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+                LoginActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
